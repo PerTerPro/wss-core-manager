c862f38cd039e76cc3e680099b98d9b149687d1c
xuantrangk54 <xuantrangk54@gmail.com>
2017-03-14 15:27:53 +0700

    Add dapper to dll

---
+++ Wss.Lib.Utilities/IWssConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wss.Lib.Utilities
+{
+    public interface IWssConverter
+    {
+        long ConvertStringToPrice(string strInput);
+    }
+}
