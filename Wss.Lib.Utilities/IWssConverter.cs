94301083a3bda72bd170eca279946ebd6929889c
Hai Nguyen <hainguyendev@gmail.com>
2017-03-24 15:07:01 +0700

    Up convertStringtoprice

--- Wss.Lib.Utilities/IWssConverter.cs
+++ Wss.Lib.Utilities/IWssConverter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,6 @@ namespace Wss.Lib.Utilities
     public interface IWssConverter
     {
         long ConvertStringToPrice(string strInput);
+        long ConvertStringToPrice(string strInput, CultureInfo cultureInfo);
     }
 }
--- Wss.Lib.Utilities/WssConverter.cs
+++ Wss.Lib.Utilities/WssConverter.cs
@@ -1,16 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Wss.Lib.Utilities
 {
     public class WssConverter : IWssConverter
     {
+        public long ConvertStringToPrice(string strInput, CultureInfo cultureInfo)
+        {
+            long price = 0;
+            if (!string.IsNullOrEmpty(strInput.Trim().ToLower()))
+            {
+                strInput = Regex.Replace(strInput.ToLower().Trim(), @"\s*vnd|\s*vnđ|\s*đ", "");
+                price = long.Parse(strInput,cultureInfo);
+            }
+            return price;
+        }
+
         public long ConvertStringToPrice(string strInput)
         {
-            throw new NotImplementedException();
+            long price = 0;
+            if (!string.IsNullOrEmpty(strInput.Trim().ToLower()))
+            {
+                strInput = Regex.Replace(strInput.ToLower().Trim(), @"\s*vnd|\s*vnđ|\s*đ", "");
+                price = long.Parse(strInput);
+            }
+            return price;
         }
     }
-}
+}  
