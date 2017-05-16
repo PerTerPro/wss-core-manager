980e69065f2c8fbe17d31b45015a7d738ecd360a
hainguyendev <hainguyendev@gmail.com>
2017-05-16 17:13:57 +0700

    a

--- Wss.Lib.Utilities/Common.cs
+++ Wss.Lib.Utilities/Common.cs
@@ -28,7 +28,7 @@ namespace Wss.Lib.Utilities
             }
             return FormatKeyword(retVal);
         }
-        private static string FormatKeyword(string s)
+        public static string FormatKeyword(string s)
         {
             string r = s;
             string[] blackword = new string[] {
@@ -45,6 +45,75 @@ namespace Wss.Lib.Utilities
             r = r.Trim().Replace("  ", " ");
             r = r.Replace('+', ' ');
             return r;
+        }
+        public static IEnumerable<string> makeNgrams(string text, int nGramSize)
+        {
+            if (nGramSize == 0) throw new Exception("nGram size was not set");
+
+            StringBuilder nGram = new StringBuilder();
+            Queue<int> wordLengths = new Queue<int>();
+
+            int wordCount = 0;
+            int lastWordLen = 0;
+
+            //append the first character, if valid.
+            //avoids if statement for each for loop to check i==0 for before and after vars.
+            if (text != "" && char.IsLetterOrDigit(text[0]))
+            {
+                nGram.Append(text[0]);
+                lastWordLen++;
+            }
+
+            //generate ngrams
+            for (int i = 1; i <= text.Length - 1; i++)
+            {
+                char before = text[i - 1];
+                char after = ' ';
+                if (i + 1 < text.Length)
+                {
+                    after = text[i + 1];
+                }
+                if (char.IsLetterOrDigit(text[i])
+                        ||
+                        //keep all punctuation that is surrounded by letters or numbers on both sides.
+                        (text[i] != ' '
+                        && (char.IsSeparator(text[i]) || char.IsPunctuation(text[i]))
+                        && (char.IsLetterOrDigit(before) && char.IsLetterOrDigit(after))
+                        )
+                    )
+                {
+                    nGram.Append(text[i]);
+                    lastWordLen++;
+                }
+                else
+                {
+                    if (lastWordLen > 0)
+                    {
+                        wordLengths.Enqueue(lastWordLen);
+                        lastWordLen = 0;
+                        wordCount++;
+
+                        if (wordCount >= nGramSize)
+                        {
+                            yield return nGram.ToString();
+                            var dqLength = (int)wordLengths.Dequeue() + 1;
+                            if (dqLength <= nGram.Length)
+                            {
+                                nGram.Remove(0, dqLength);
+                            }
+                            else nGram.Remove(0, dqLength - 1);
+                            wordCount -= 1;
+                        }
+                        if (nGramSize > 1)
+                        {
+                            nGram.Append(" ");
+                        }
+
+                    }
+                }
+            }
+
+            yield return nGram.ToString();
         }
     }
 }
